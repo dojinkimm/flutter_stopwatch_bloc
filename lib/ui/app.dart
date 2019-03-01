908e9b2eb24b34fd17c2ecbfa816bd953d471b62
Henry <dojinkim119@gmail.com>
2019-03-01 20:44:51 +0900

    stopwatch app setting

---
+++ lib/ui/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './home.dart';
+
+class RunApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "StopWatch App", home: HomePage());
+  }
+}

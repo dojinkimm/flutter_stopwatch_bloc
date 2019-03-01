7c54acebb3b271dd44a820be19af1f1230d41758
Henry <dojinkim119@gmail.com>
2019-03-01 20:45:24 +0900

    model controlling the time

---
+++ lib/model/elapsed_time.dart
@@ -0,0 +1,9 @@
+class ElapsedTime {
+  final String hundreds;
+  final String seconds;
+  final String minutes;
+  final bool isRunning;
+
+  ElapsedTime(
+      {this.hundreds, this.seconds, this.minutes, this.isRunning});
+}
